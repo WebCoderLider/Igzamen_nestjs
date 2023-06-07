0a9759954f255e9ba5492f418e618e07462b0d4c
WebCoderLider <ilhomjoniskandarov2006@gmail.com>
2023-06-07 11:04:18 +0500

    crud qoshildi

--- src/Cars/cars.controllers.ts
+++ src/Cars/cars.controllers.ts
@@ -1,7 +1,27 @@
-import { Controller, Get, Req, Post, Body, Delete, Param, Put } from '@nestjs/common';
-import { Request } from 'express';
+import { Controller, Get, Req, Post, Body, Delete, Param, Put, UseInterceptors, UploadedFile, Res, HttpStatus, InternalServerErrorException } from '@nestjs/common';
+import { Request, Response } from 'express';
 import { Pool } from 'pg';
+import { FileInterceptor } from '@nestjs/platform-express';
+import * as path from 'path';
+import * as fs from 'fs/promises';
+import * as imgfs from 'fs';
 import * as uuid from 'uuid';
+
+interface Car {
+  cars_marka: string;
+  cars_tanirovka: string;
+  cars_motor: string;
+  cars_year: number;
+  cars_color: string;
+  cars_distance: string;
+  cars_gearbook: string;
+  cars_description: string;
+  cars_img: File;
+  category_id: string;
+  car_price: number; // Add car_price field
+}
+
+
 
 @Controller('cars')
 export class CarsController {
@@ -17,51 +37,36 @@ export class CarsController {
     });
   }
 
+
+  @Get('/:car_id') // "car" so'zini qo'shing
+  async getCar(@Param('car_id') car_id: string): Promise<Car> {
+    const client = await this.pool.connect();
+    try {
+      const result = await client.query(
+        `
+        SELECT *
+        FROM cars
+        WHERE car_id = $1
+        `,
+        [car_id],
+      );
+      return result.rows[0]; // Ma'lumotlarni qaytarish
+    } catch (error) {
+      console.error('Error executing query', error);
+      throw new InternalServerErrorException('An error occurred');
+    } finally {
+      client.release();
+    }
+  }
+
+
+
   @Get()
   async findAll(@Req() request: Request): Promise<string> {
     const client = await this.pool.connect();
     try {
       const result = await client.query('SELECT * FROM cars');
       return JSON.stringify(result.rows);
-    } catch (error) {
-      console.error('Error executing query', error);
-      return 'An error occurred';
-    } finally {
-      client.release();
-    }
-  }
-
-  @Post()
-  async create(@Body() body: {
-    cars_marka: string,
-    cars_tanirovka: string,
-    cars_motor: string,
-    cars_year: string,
-    cars_color: string,
-    cars_distance: string,
-    cars_gearbook: string,
-    cars_description: string,
-    cars_img: string,
-  }): Promise<string> {
-    const {
-      cars_marka,
-      cars_tanirovka,
-      cars_motor,
-      cars_year,
-      cars_color,
-      cars_distance,
-      cars_gearbook,
-      cars_description,
-      cars_img,
-    } = body;
-    const id = uuid.v4();
-    const client = await this.pool.connect();
-    try {
-      await client.query(
-        'INSERT INTO cars (car_id, cars_marka, cars_tanirovka, cars_motor, cars_year, cars_color, cars_distance, cars_gearbook, cars_description, cars_img) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)',
-        [id, cars_marka, cars_tanirovka, cars_motor, cars_year, cars_color, cars_distance, cars_gearbook, cars_description, cars_img]
-      );
-      return 'Car created successfully';
     } catch (error) {
       console.error('Error executing query', error);
       return 'An error occurred';
@@ -85,17 +90,20 @@ export class CarsController {
   }
 
   @Put(':id')
-  async update(@Param('id') id: string, @Body() body: {
-    cars_marka?: string,
-    cars_tanirovka?: string,
-    cars_motor?: string,
-    cars_year?: string,
-    cars_color?: string,
-    cars_distance?: string,
-    cars_gearbook?: string,
-    cars_description?: string,
-    cars_img?: string,
-  }): Promise<string> {
+  async update(
+    @Param('id') id: string,
+    @Body() body: {
+      cars_marka?: string;
+      cars_tanirovka?: string;
+      cars_motor?: string;
+      cars_year?: string;
+      cars_color?: string;
+      cars_distance?: string;
+      cars_gearbook?: string;
+      cars_description?: string;
+      cars_img?: string;
+    }
+  ): Promise<string> {
     const client = await this.pool.connect();
     try {
       let updateQuery = 'UPDATE cars SET';
@@ -107,7 +115,7 @@ export class CarsController {
           values.push(body[key]);
         }
       });
-      updateQuery = updateQuery.slice(0, -1); // So'nggi vergulni olib tashlash
+      updateQuery = updateQuery.slice(0, -1); // Remove the last comma
       values.push(id);
       await client.query(updateQuery + ' WHERE car_id = $' + values.length, values);
       return 'Car updated successfully';
@@ -117,5 +125,102 @@ export class CarsController {
     } finally {
       client.release();
     }
+  }
+
+
+
+  @Get('img/:cars_img')
+  async getCarsImage(@Param('cars_img') carsImg: string, @Res() res: Response): Promise<void> {
+    const client = await this.pool.connect();
+    try {
+      const result = await client.query(
+        `
+        SELECT cars_img
+        FROM cars
+        WHERE cars_img LIKE $1
+      `,
+        [`%${carsImg}%`],
+      );
+
+      if (result.rows.length > 0) {
+        const imagePath = path.join(__dirname, '..', '..', 'uploads', result.rows[0].cars_img);
+
+        const fileStream = imgfs.createReadStream(imagePath);
+        fileStream.on('open', () => {
+          res.setHeader('Content-Type', 'image/*');
+          fileStream.pipe(res);
+        });
+        fileStream.on('error', (error) => {
+          console.error('Error reading image file', error);
+          res.status(HttpStatus.INTERNAL_SERVER_ERROR).send('An error occurred');
+        });
+      } else {
+        res.status(HttpStatus.NOT_FOUND).send('Car image not found');
+      }
+    } catch (error) {
+      console.error('Error executing query', error);
+      res.status(HttpStatus.INTERNAL_SERVER_ERROR).send('An error occurred');
+    } finally {
+      client.release();
+    }
+  }
+
+
+
+
+  @Post()
+  @UseInterceptors(FileInterceptor('cars_img'))
+  async create(
+    @Body() body: {
+      cars_marka: string;
+      cars_tanirovka: string;
+      cars_motor: string;
+      cars_year: number; // Updated field type to number
+      cars_color: string;
+      cars_distance: string;
+      cars_gearbook: string;
+      cars_description: string;
+      cars_img: string; // Updated field type to string
+      category_id: string;
+      car_price: number; // Add car_price field
+    },
+    @UploadedFile() file: Express.Multer.File
+  ): Promise<string> {
+    const client = await this.pool.connect();
+    try {
+      const {
+        cars_marka,
+        cars_tanirovka,
+        cars_motor,
+        cars_year,
+        cars_color,
+        cars_distance,
+        cars_gearbook,
+        cars_description,
+        category_id,
+        car_price
+      } = body;
+
+      const car_id = uuid.v4(); // Generate a UUID for car_id field
+
+      // Handle file upload using Multer
+      const fileName = car_id + path.extname(file.originalname);
+      const filePath = path.join(__dirname, '..', '..', 'uploads', fileName);
+      await fs.writeFile(filePath, file.buffer);
+
+      const result = await client.query(
+        'INSERT INTO cars (car_id, cars_marka, cars_tanirovka, cars_motor, cars_year, cars_color, cars_distance, cars_gearbook, cars_description, cars_img, car_price, category_id) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12) RETURNING *',
+        [car_id, cars_marka, cars_tanirovka, cars_motor, cars_year, cars_color, cars_distance, cars_gearbook, cars_description, fileName, car_price, category_id]
+      )
+
+      return 'Car created successfully';
+    } catch (error) {
+      console.error('Error executing query', error);
+      return 'An error occurred';
+    } finally {
+      client.release();
+    }
+
+
   }
 }
--- src/Categoriya/categorys.controllers.ts
+++ src/Categoriya/categorys.controllers.ts
@@ -1,7 +1,10 @@
-import { Controller, Get, Req, Post, Body, Delete, Param, Put } from '@nestjs/common';
-import { Request } from 'express';
+import { Controller, Get, Req, Post, Body, Delete, Param, Put, UploadedFile, UseInterceptors, Res, HttpStatus } from '@nestjs/common';
+import { Request, Response } from 'express';
+import { FileInterceptor } from '@nestjs/platform-express';
 import { Pool } from 'pg';
 import * as uuid from 'uuid';
+import fs from 'fs';
+import path from 'path';
 
 @Controller('categories')
 export class CategoryController {
@@ -17,12 +20,81 @@ export class CategoryController {
     });
   }
 
+
+  @Get('/uploads/:category_img') // URL manzilini "uploads/:category_img" ga o'zgartirish
+  async getCategoryImage(@Param('category_img') categoryImg: string, @Res() res: Response): Promise<void> {
+    const client = await this.pool.connect();
+    try {
+      const result = await client.query(
+        `
+      SELECT category_img
+      FROM categories
+      WHERE category_img LIKE $1
+    `,
+        [`%uploads/${categoryImg}%`], // Fayl nomini qidirish uchun "uploads/:category_img" ga o'zgartirish
+      );
+
+      if (result.rows.length > 0) {
+        const imagePath = path.join(__dirname, '..', '..', result.rows[0].category_img);
+        const imageStream = fs.createReadStream(imagePath);
+        imageStream.pipe(res);
+      } else {
+        res.status(HttpStatus.NOT_FOUND).send('Category image not found');
+      }
+    } catch (error) {
+      console.error('Error executing query', error);
+      res.status(HttpStatus.INTERNAL_SERVER_ERROR).send('An error occurred');
+    } finally {
+      client.release();
+    }
+  }
+
+
   @Get()
   async findAll(@Req() request: Request): Promise<string> {
     const client = await this.pool.connect();
     try {
-      const result = await client.query('SELECT * FROM categories');
-      return JSON.stringify(result.rows);
+      const result = await client.query(`
+        SELECT categories.*, json_agg(cars.*) AS cars
+        FROM categories
+        LEFT JOIN cars ON categories.category_id = cars.category_id
+        GROUP BY categories.category_id
+      `);
+      return JSON.stringify(result.rows, null, 2);
+    } catch (error) {
+      console.error('Error executing query', error);
+      return 'An error occurred';
+    } finally {
+      client.release();
+    }
+  }
+
+
+
+
+
+
+
+  @Get(':category_id')
+  async findOne(@Param('category_id') categoryId: string): Promise<string> {
+    const client = await this.pool.connect();
+    try {
+      const result = await client.query(
+        `
+          SELECT categories.*, json_agg(cars.*) AS cars
+          FROM categories
+          LEFT JOIN cars ON categories.category_id = cars.category_id
+          WHERE categories.category_id = $1
+          GROUP BY categories.category_id
+        `,
+        [categoryId],
+      );
+
+      if (result.rows.length > 0) {
+        return JSON.stringify(result.rows[0], null, 2);
+      } else {
+        return 'Category not found';
+      }
     } catch (error) {
       console.error('Error executing query', error);
       return 'An error occurred';
@@ -32,12 +104,27 @@ export class CategoryController {
   }
 
   @Post()
-  async create(@Body() body: { category_name: string, category_img: string }): Promise<string> {
-    const { category_name, category_img } = body;
+  @UseInterceptors(FileInterceptor('category_img'))
+  async create(@Req() request: Request, @UploadedFile() categoryImg: Express.Multer.File): Promise<string> {
+    const { category_name } = request.body;
     const id = uuid.v4();
     const client = await this.pool.connect();
     try {
-      await client.query('INSERT INTO categories (category_id, category_name, category_img) VALUES ($1, $2, $3)', [id, category_name, category_img]);
+      let categoryImgPath: string = null; // Fayl nomini saqlash uchun o'zgaruvchi
+
+      if (categoryImg) {
+        const fileExtension = categoryImg.originalname.split('.').pop();
+        const uploadPath = `uploads/${id}.${fileExtension}`;
+        fs.writeFileSync(uploadPath, categoryImg.buffer);
+        categoryImgPath = uploadPath;
+      }
+
+      await client.query('INSERT INTO categories (category_id, category_name, category_img) VALUES ($1, $2, $3)', [
+        id,
+        category_name,
+        categoryImgPath, // Fayl nomini ma'lumotlar bazasiga saqlang
+      ]);
+
       return 'Category created successfully';
     } catch (error) {
       console.error('Error executing query', error);
@@ -46,6 +133,7 @@ export class CategoryController {
       client.release();
     }
   }
+
 
   @Delete(':id')
   async delete(@Param('id') id: string): Promise<string> {
@@ -62,11 +150,24 @@ export class CategoryController {
   }
 
   @Put(':id')
-  async update(@Param('id') id: string, @Body() body: { category_name: string, category_img: string }): Promise<string> {
+  async update(
+    @Param('id') id: string,
+    @Body() body: { category_name: string; category_img?: string },
+  ): Promise<string> {
     const { category_name, category_img } = body;
     const client = await this.pool.connect();
     try {
-      await client.query('UPDATE categories SET category_name = $1, category_img = $2 WHERE category_id = $3', [category_name, category_img, id]);
+      if (category_img) {
+        const fileExtension = category_img.split('.').pop();
+        const newCategoryImg = `${uuid.v4()}.${fileExtension}`;
+        await client.query('UPDATE categories SET category_name = $1, category_img = $2 WHERE category_id = $3', [
+          category_name,
+          newCategoryImg,
+          id,
+        ]);
+      } else {
+        await client.query('UPDATE categories SET category_name = $1 WHERE category_id = $2', [category_name, id]);
+      }
       return 'Category updated successfully';
     } catch (error) {
       console.error('Error executing query', error);
